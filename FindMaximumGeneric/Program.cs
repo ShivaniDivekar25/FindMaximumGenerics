2687d2c3aa28c685eb8c4972f334c7a562ec412f
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-21 11:55:26 +0530

    Modified to Find Maximum Integer

--- FindMaximumGeneric/Program.cs
+++ FindMaximumGeneric/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to find maximum using generics");
+            Console.WriteLine("Please enter an option");
+            Console.WriteLine("1:Find maximum integer");
+            int option = Convert.ToInt32(Console.ReadLine());
+            FindMaximum findMaximum = new FindMaximum();
+            switch (option)
+            {
+                case 1:
+                    findMaximum.FindMaxInt(12, 36, 46);
+                    break;
+                default:
+                    Console.WriteLine("Please enter right option");
+                    break;
+            }
         }
     }
 }
